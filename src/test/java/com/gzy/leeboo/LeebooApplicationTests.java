6b325a0eb73463a79ea864982f597e01e41b2a34
gaoziyang <751928542@qq.com>
2020-10-29 13:45:44 +0800

    [2020-10-29] 新增内容： 1.使用Nginx部署，访问域名www.summ1t.top。

--- src/test/java/com/gzy/leeboo/LeebooApplicationTests.java
+++ src/test/java/com/gzy/leeboo/LeebooApplicationTests.java
@@ -1,34 +1,14 @@
 package com.gzy.leeboo;
 
-import com.aliyuncs.CommonRequest;
-import com.aliyuncs.CommonResponse;
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
-import com.aliyuncs.http.MethodType;
-import com.aliyuncs.profile.DefaultProfile;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gzy.leeboo.config.aliyun.Aliyun;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.servlet.http.HttpServlet;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
-
 @SpringBootTest
-class LeebooApplicationTests extends HttpServlet {
+class LeebooApplicationTests {
     @Test
     void contextLoads() {
-
-    }
-
-    @Test
-    public void UUIDTest() {
+        System.out.println(2 << 8);
 
     }
 }
